f3d4b41478db9ba7c653623475d0fef936b1b6fb
Phuong Vo <phuong.vo@eightcap.com>
2020-03-15 01:58:10 +0700

    remove unused code

--- src/index.js
+++ src/index.js
@@ -4,21 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import NameForm from './Forms/NameForm';
 
-const numbers = [1, 2, 3, 4, 5, 6];
-function NumberList(props) {
-    return (
-        <ul>{props.numbers.map((number) =>
-            <li key={number.toString()}>{number * 2}</li>)}
-        </ul>
-    );
-}
 
-
-const posts = [
-    { id: 1, title: 'Hello 1', content: 'Content 1' },
-    { id: 2, title: 'Hello 2', content: 'Content 2' },
-
-]
 ReactDOM.render(<NameForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
